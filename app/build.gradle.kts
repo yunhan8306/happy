a835105cdcb93febb15a9ad43c87339696c2ff64
yunhan8306 <dbsgks17@naver.com>
2024-10-05 23:58:17 +0900

    Feat: application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/gradle/incremental.annotation.processors"
         }
     }
 }
@@ -64,6 +65,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.dagger.hilt.android)
+    implementation(libs.dagger.hilt.android.compiler)
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
 

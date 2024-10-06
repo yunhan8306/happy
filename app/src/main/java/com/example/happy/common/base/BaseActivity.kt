72ed4c3bbb9cf5a9b0ca0f4ed8d634e829727b5c
yunhan8306 <dbsgks17@naver.com>
2024-10-06 13:32:59 +0900

    Feat: BaseActivity

---
+++ app/src/main/java/com/example/happy/common/base/BaseActivity.kt
@@ -0,0 +1,26 @@
+package com.example.happy.common.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleOwner
+import com.example.happy.common.util.LifecycleOwnerWrapper
+
+abstract class BaseActivity<Binding : ViewDataBinding> : AppCompatActivity(),
+    LifecycleOwnerWrapper {
+
+    protected val binding: Binding by lazy { createBinding() }
+
+    protected abstract fun createBinding(): Binding
+
+    protected open fun initActivity(savedInstanceState: Bundle?) = Unit
+
+    override fun initLifecycleOwner(): LifecycleOwner = this
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.lifecycleOwner = this
+        initActivity(savedInstanceState)
+    }
+}
